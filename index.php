c7fd597cbad2c3f230726ca13d327d5a2b6a1a1f
ernil <morion.ernil@gmail.com>
2014-11-07 17:52:10 +0300

    update without mysql

--- index.php
+++ index.php
@@ -4,11 +4,11 @@ $host = 'localhost';
 $user = 'ely';
 $pass = '11111';
 
-$link = mysql_connect($host, $user, $pass);
-if (!$link) {
-    die('Ошибка соединения: ' . mysql_error());
-}
+// $link = mysql_connect($host, $user, $pass);
+// if (!$link) {
+//     die('Ошибка соединения: ' . mysql_error());
+// }
 echo 'Успешно соединились';
-mysql_close($link);
+// mysql_close($link);
 
 ?>
