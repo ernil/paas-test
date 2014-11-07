99673b29b2674ab2d01cf3cc2f7f7ac0b61d05ac
ernil <morion.ernil@gmail.com>
2014-11-07 16:57:08 +0300

    new php test app

---
+++ web/index.php
@@ -0,0 +1,14 @@
+<? 
+
+$host = 'localhost';
+$user = 'ely';
+$pass = '11111';
+
+$link = mysql_connect($host, $user, $pass);
+if (!$link) {
+    die('Ошибка соединения: ' . mysql_error());
+}
+echo 'Успешно соединились';
+mysql_close($link);
+
+?>
