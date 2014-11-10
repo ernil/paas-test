b9601c1e5ca323c68cfd8f1e231b7e6f0325ee0c
ernil <morion.ernil@gmail.com>
2014-11-10 17:43:12 +0300

    111

--- index.php
+++ index.php
@@ -1,31 +1,32 @@
 <? 
 
-use Herrera\Pdo\PdoServiceProvider;
-use Silex\Application;
+// use Herrera\Pdo\PdoServiceProvider;
+// use Silex\Application;
 
-error_reporting(E_ERROR | E_WARNING | E_PARSE);
+// error_reporting(E_ERROR | E_WARNING | E_PARSE);
 
-$app = new Application();
+// $app = new Application();
 
 $db_url = getenv('DB_URL');
 
 echo $db_url;
-$dbopts = parse_url(getenv($db_url));
 
-$host = 'localhost';
-$user = 'ely';
-$pass = '11111';
+// $dbopts = parse_url(getenv($db_url));
 
-print_r($dbopts);
+// $host = 'localhost';
+// $user = 'ely';
+// $pass = '11111';
+
+// print_r($dbopts);
 
 
-$app->register(new Herrera\Pdo\PdoServiceProvider(),
-  array(
-    'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"],
-    'pdo.port' => $dbopts["port"],
-    'pdo.username' => $dbopts["user"],
-    'pdo.password' => $dbopts["pass"]
-  )
-);
+// $app->register(new Herrera\Pdo\PdoServiceProvider(),
+//   array(
+//     'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"],
+//     'pdo.port' => $dbopts["port"],
+//     'pdo.username' => $dbopts["user"],
+//     'pdo.password' => $dbopts["pass"]
+//   )
+// );
 
 ?>
