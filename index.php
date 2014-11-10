485d6fe76a11381762e3942f9ec9f898d53383b7
ernil <morion.ernil@gmail.com>
2014-11-10 17:18:54 +0300

    revdfvdf

--- index.php
+++ index.php
@@ -4,11 +4,21 @@ $host = 'localhost';
 $user = 'ely';
 $pass = '11111';
 
-// $link = mysql_connect($host, $user, $pass);
-// if (!$link) {
-//     die('Ошибка соединения: ' . mysql_error());
-// }
-echo 'Успешно соединились';
-// mysql_close($link);
+use Herrera\Pdo\PdoServiceProvider;
+use Silex\Application;
+
+$app = new Application();
+
+$dbopts = parse_url(getenv('DATABASE_URL'));
+print_r($dbopts);
+
+$app->register(new Herrera\Pdo\PdoServiceProvider(),
+  array(
+    'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"],
+    'pdo.port' => $dbopts["port"],
+    'pdo.username' => $dbopts["user"],
+    'pdo.password' => $dbopts["pass"]
+  )
+);
 
 ?>
